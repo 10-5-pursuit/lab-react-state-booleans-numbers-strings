333a6aa38c22784fba5bbb0d773b634e72f13304
MiloCo86 <cpaez@pursuit.org>
2024-04-16 10:30:24 -0400

    component completed and fully functional

--- src/Components/ClickGame.jsx
+++ src/Components/ClickGame.jsx
@@ -1,22 +1,54 @@
+import { useState } from "react";
 import "./ClickGame.css";
-
-let score = 0;
-let increment = 1
-
 
 
 function ClickGame(){
+    const [score, setScore] = useState(0);
+    const [increment, setIncrement] = useState(1);
+    const [gameBtnStyle, setGameBtnStyle] = useState('block')
+    const [winElements, setgamebtnStyle] = useState('none')
+   
 
-    
-    
+    function PayTen (){
+        if(score<10){
+            alert("You can't affort that")
+        }else{
+            setScore(score-10);
+            setIncrement(increment+1); 
+        }
+    }
+    function checkScore(){
+        setScore(score + increment)
+        if(score + increment>=100){
+            setGameBtnStyle('none');
+            setgamebtnStyle('block');
+        }
+    }
+
+    function resetGame(){
+        setScore(0);
+        setIncrement(1);
+        setGameBtnStyle('block');
+        setgamebtnStyle('none');
+    }
+
     return (
         <div className="container">
             <h1>Current Score: {score}</h1>
-            <button className="increment" onClick={()=>{
-                score += increment;
-                console.log(score)
-            }} >+{increment}</button>
-            <button className="pay_btn">Pay 10 points to change from +{increment} to +{increment+1}</button>
+
+            <h1 style={{'display':winElements}}>YOU WIN!!!</h1>
+
+            <button className="increment" 
+            onClick={checkScore} 
+            style={{'display':gameBtnStyle}}>+{increment}</button>
+
+            <button className="pay_btn" 
+            onClick={PayTen}
+            style={{'display':gameBtnStyle}}>Pay 10 points to change from +{increment} to +{increment+1}</button>
+            
+            <button className="win_btn" 
+            onClick={resetGame} 
+            style={{'display':winElements}}>Play again</button>
         </div>
     )
 }
