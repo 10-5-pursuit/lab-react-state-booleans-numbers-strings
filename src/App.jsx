7818d4fb1226d1d06c59e5c40323e24cf213e1cf
sadya104 <sandrickson@pursuit.com>
2024-05-11 16:19:17 -0400

    updated count

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React , {useState} from "react";
+import React , { useState, useEffect } from "react";
 import "./App.css";
 
 
 function App () {
 
-  const [count , setCount] = useState(0)
+  const [count , setCount] = useState(1)
+  const [secondCount , setSecondCount] = useState(1)
   
+
+  const updateCount = () => {
+    setCount(count + secondCount);
+  }
+
+  const payTenPoints = () => {
+    if(count >= 10) {
+      setCount(count - 10)
+      setSecondCount(secondCount + 1)
+    }
+    else if (count < 10 ) {
+      alert('You can\'t afford that!')
+    }
+    
+  }
+
     return (
+      
       <main>
         <h1>Current Score: {count} </h1>
-        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={updateCount}>{secondCount}</button>
+        <button className="longButton" onClick={payTenPoints} >Pay 10 points to change from +{secondCount} to +{secondCount + 1}</button>
       </main>
     );
 }
